1aa794b69234670112a5e47cc89c14a91beb73e1
Fauziakbar101 <fakabr7@gmail.com>
2018-11-03 23:01:34 +0700

    Uts Praktikum

---
+++ Soal1/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+ int main()
+{
+    int a,b,x,y;
+    cout<<"Program Hitungan"<<endl<<endl;
+    cout<<"Input A : ";
+    cin>>a;
+    cout<<"Input B : ";
+    cin>>b;
+    x=a;
+    y=b;
+     while (x!=y)
+            if (x<y){x=x+a;
+            }
+            else
+            {y=y+b;}
+                cout<<endl;
+    cout<<"Nilai X = "<<x;
+    return 0;
+}
